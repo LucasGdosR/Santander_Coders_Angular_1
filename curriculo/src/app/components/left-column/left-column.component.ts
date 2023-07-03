c4caf99984748a8d81eb7aee489d0c8fb1859cad
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2023-07-03 13:42:56 -0300

    Implementa coluna da esquerda, organiza tudo

--- curriculo/src/app/components/left-column/left-column.component.ts
+++ curriculo/src/app/components/left-column/left-column.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./left-column.component.css']
 })
 export class LeftColumnComponent {
+  public fields = [{ 
+    text: 'Designer',
+    icon: 'fa-briefcase'
+  }, {
+    text: 'London, UK',
+    icon: 'fa-home'
+  }, {
+    text: 'ex@mail.com',
+    icon: 'fa-envelope'
+  }, {
+    text: '1224435534',
+    icon: 'fa-phone'
+  }]
 
+  public skills = [{
+    text: 'Adobe Photoshop',
+    percentage: 90
+  }, {
+    text: 'Photography',
+    percentage: 80
+  }, {
+    text: 'Illustrator',
+    percentage: 75
+  }, {
+    text: 'Media',
+    percentage: 50
+  }]
+
+  public languages = [{
+    text: 'English',
+    width: 100
+  }, {
+    text: 'Spanish',
+    width: 50
+  }, {
+    text: 'German',
+    width: 25
+  }]
 }
